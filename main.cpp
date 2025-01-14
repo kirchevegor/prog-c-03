5f3e98fb6f83422df79c3393d910598a6a4f4109
kaneare <kirchevegor31@gmail.com>
2025-01-14 14:33:52 +0700

    Сохранение изменений перед pull

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdexcept>
-#include <algorithm>
 
+// Последовательный контейнер (массив)
 template <typename T>
 class SequentialContainer {
 private:
@@ -69,35 +69,237 @@ public:
     }
 };
 
-int main() {
-    SequentialContainer<int> seqCont;
+// Двунаправленный связанный список
+template <typename T>
+class DoublyLinkedList {
+private:
+    struct Node {
+        T data;
+        Node* prev;
+        Node* next;
 
-    // Добавление элементов
+        Node(const T& value) : data(value), prev(nullptr), next(nullptr) {}
+    };
+
+    Node* head;
+    Node* tail;
+    size_t size;
+
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr), size(0) {}
+
+    ~DoublyLinkedList() {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    void push_back(const T& value) {
+        Node* newNode = new Node(value);
+        if (tail) {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        } else {
+            head = tail = newNode;
+        }
+        ++size;
+    }
+
+    void insert(size_t index, const T& value) {
+        if (index > size) throw std::out_of_range("Index out of range");
+        Node* newNode = new Node(value);
+        if (index == 0) {
+            newNode->next = head;
+            if (head) head->prev = newNode;
+            head = newNode;
+        } else {
+            Node* current = head;
+            for (size_t i = 0; i < index - 1; ++i) {
+                current = current->next;
+            }
+            newNode->next = current->next;
+            if (current->next) current->next->prev = newNode;
+            current->next = newNode;
+            newNode->prev = current;
+        }
+        if (!newNode->next) tail = newNode;
+        ++size;
+    }
+
+    void erase(size_t index) {
+        if (index >= size) throw std::out_of_range("Index out of range");
+        Node* current = head;
+        for (size_t i = 0; i < index; ++i) {
+            current = current->next;
+        }
+        if (current->prev) current->prev->next = current->next;
+        if (current->next) current->next->prev = current->prev;
+        if (current == head) head = current->next;
+        if (current == tail) tail = current->prev;
+        delete current;
+        --size;
+    }
+
+    void print() const {
+        Node* current = head;
+        while (current) {
+            std::cout << current->data << (current->next ? ", " : "\n");
+            current = current->next;
+        }
+    }
+
+    size_t get_size() const {
+        return size;
+    }
+};
+
+// Односвязный связанный список
+template <typename T>
+class SinglyLinkedList {
+private:
+    struct Node {
+        T data;
+        Node* next;
+
+        Node(const T& value) : data(value), next(nullptr) {}
+    };
+
+    Node* head;
+    size_t size;
+
+public:
+    SinglyLinkedList() : head(nullptr), size(0) {}
+
+    ~SinglyLinkedList() {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    void push_back(const T& value) {
+        Node* newNode = new Node(value);
+        if (!head) {
+            head = newNode;
+        } else {
+            Node* current = head;
+            while (current->next) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+        ++size;
+    }
+
+    void insert(size_t index, const T& value) {
+        if (index > size) throw std::out_of_range("Index out of range");
+        Node* newNode = new Node(value);
+        if (index == 0) {
+            newNode->next = head;
+            head = newNode;
+        } else {
+            Node* current = head;
+            for (size_t i = 0; i < index - 1; ++i) {
+                current = current->next;
+            }
+            newNode->next = current->next;
+            current->next = newNode;
+        }
+        ++size;
+    }
+
+    void erase(size_t index) {
+        if (index >= size) throw std::out_of_range("Index out of range");
+        Node* current = head;
+        if (index == 0) {
+            head = current->next;
+            delete current;
+        } else {
+            Node* prev = nullptr;
+            for (size_t i = 0; i < index; ++i) {
+                prev = current;
+                current = current->next;
+            }
+            prev->next = current->next;
+            delete current;
+        }
+        --size;
+    }
+
+    void print() const {
+        Node* current = head;
+        while (current) {
+            std::cout << current->data << (current->next ? ", " : "\n");
+            current = current->next;
+        }
+    }
+
+    size_t get_size() const {
+        return size;
+    }
+};
+
+int main() {
+    // Демонстрация для SequentialContainer
+    std::cout << "SequentialContainer:" << std::endl;
+    SequentialContainer<int> seqCont;
     for (int i = 0; i < 10; ++i) {
         seqCont.push_back(i);
     }
-
-    // Вывод содержимого
+    seqCont.print();
+    std::cout << "Size: " << seqCont.get_size() << std::endl;
+    seqCont.erase(2);
+    seqCont.erase(4);
+    seqCont.erase(5);
+    seqCont.print();
+    std::cout << "Size: " << seqCont.get_size() << std::endl;
+    seqCont.insert(0, 10);
+    seqCont.insert(4, 20);
+    seqCont.push_back(30);
     seqCont.print();
     std::cout << "Size: " << seqCont.get_size() << std::endl;
 
-    // Удаление элементов
-    seqCont.erase(2); // Удаление 3-го
-    seqCont.erase(4); // Удаление 5-го
-    seqCont.erase(5); // Удаление 7-го (сдвиг)
+    // Демонстрация для DoublyLinkedList
+    std::cout << "\nDoublyLinkedList:" << std::endl;
+    DoublyLinkedList<int> doublyList;
+    for (int i = 0; i < 10; ++i) {
+        doublyList.push_back(i);
+    }
+    doublyList.print();
+    std::cout << "Size: " << doublyList.get_size() << std::endl;
+    doublyList.erase(2);
+    doublyList.erase(4);
+    doublyList.erase(5);
+    doublyList.print();
+    std::cout << "Size: " << doublyList.get_size() << std::endl;
+    doublyList.insert(0, 10);
+    doublyList.insert(4, 20);
+    doublyList.push_back(30);
+    doublyList.print();
+    std::cout << "Size: " << doublyList.get_size() << std::endl;
 
-    // Вывод содержимого
-    seqCont.print();
-    std::cout << "Size: " << seqCont.get_size() << std::endl;
-
-    // Добавление элементов
-    seqCont.insert(0, 10); // В начало
-    seqCont.insert(4, 20); // В середину
-    seqCont.push_back(30); // В конец
-
-    // Вывод содержимого
-    seqCont.print();
-    std::cout << "Size: " << seqCont.get_size() << std::endl;
+    // Демонстрация для SinglyLinkedList
+    std::cout << "\nSinglyLinkedList:" << std::endl;
+    SinglyLinkedList<int> singlyList;
+    for (int i = 0; i < 10; ++i) {
+        singlyList.push_back(i);
+    }
+    singlyList.print();
+    std::cout << "Size: " << singlyList.get_size() << std::endl;
+    singlyList.erase(2);
+    singlyList.erase(4);
+    singlyList.erase(5);
+    singlyList.print();
+    std::cout << "Size: " << singlyList.get_size() << std::endl;
+    singlyList.insert(0, 10);
+    singlyList.insert(4, 20);
+    singlyList.push_back(30);
+    singlyList.print();
+    std::cout << "Size: " << singlyList.get_size() << std::endl;
 
     return 0;
 }
